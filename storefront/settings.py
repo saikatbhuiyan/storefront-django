19c0ac1ead16898ef8be3f2cbbb7f8ab23538117
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-09 03:42:13 +0600

    connect with mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -91,8 +91,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'mypassword'
     }
 }
 

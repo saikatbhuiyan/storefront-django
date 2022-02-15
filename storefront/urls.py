67a663851960b566f933325a00e2b2340fb001f8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-16 05:30:00 +0600

    change admin header

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Storefront Admin"
+admin.site.index_title = "Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),

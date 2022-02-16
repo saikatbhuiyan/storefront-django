abd6ec35ec79d95a8897c152b69f70679976f29c
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-17 04:41:19 +0600

    customized admin list page

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Collection)
-admin.site.register(models.Product)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    list_select_related = ['collection']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name',  'membership', 'orders']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10

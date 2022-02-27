7b8034870fd54c1341d776259dff90662e8a675b
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-27 13:21:25 +0600

    rename app store_custom to core

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StoreCustomConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'store_custom'
+    name = 'core'
--- storefront/settings.py
+++ storefront/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "store",
     "tags",
     "likes",
-    "store_custom"
+    "core"
 ]
 
 

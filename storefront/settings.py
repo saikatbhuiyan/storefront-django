55f93a322b9869165c08f997b18e1bf355dfdc24
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-10 22:50:33 +0600

    EmailBackend setting

--- storefront/settings.py
+++ storefront/settings.py
@@ -182,3 +182,10 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@buy.com'

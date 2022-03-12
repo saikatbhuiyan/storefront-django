e86414657c4addbcad3a7e1bd92f3592c925c509
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-12 23:00:48 +0600

    create simple test_if_user_is_anonymous_returns_401

---
+++ store/tests/test_collections.py
@@ -0,0 +1,15 @@
+from urllib import response
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class TestCreateCollection:
+    def test_if_user_is_anonymous_returns_401(self):
+        # Arrange
+
+        # Act
+        client = APIClient()
+        response = client.post('/store/collections', {'title': 'a'})
+
+        # Assert
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED

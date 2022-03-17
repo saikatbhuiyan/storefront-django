5d74ee5395aa0a1a7e1f40f1732949ce589a1bc7
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-18 01:03:00 +0600

    use model-bakery for TestRetrieveCollection

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,5 +1,7 @@
+from store.models import Collection, Product
 from rest_framework import status
 import pytest
+from model_bakery import baker
 
 
 @pytest.fixture
@@ -38,3 +40,18 @@ class TestCreateCollection:
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }

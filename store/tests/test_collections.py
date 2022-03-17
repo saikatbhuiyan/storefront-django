1926d50e539245c9f82a1fcccdfdd93fa250feda
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-17 23:32:13 +0600

    test_if_data_is_invalid_returns_400 | test_if_data_is_valid_returns_201

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 
@@ -23,6 +24,25 @@ class TestCreateCollection:
         response = client.post('/store/collections', {'title': 'a'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+
+        response = client.post('/store/collections', {'title': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+
+        response = client.post('/store/collections', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
 
 
 """

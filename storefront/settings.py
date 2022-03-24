68ad24f7deb2855cf28facf13d464ebebe9eaa70
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-24 23:56:18 +0600

    django-cashe setup

--- storefront/settings.py
+++ storefront/settings.py
@@ -212,3 +212,15 @@ CELERY_BEAT_SCHEDULE = {
         # 'kwargs': {}
     }
 }
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/2',
+        'TIMEOUT': 10 * 60,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
